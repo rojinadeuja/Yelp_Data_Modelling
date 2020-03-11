38a560c1f6df89b42172a90a9176d41cf5ace77c
rojinadeuja <rojinadeuja33g@gmail.com>
2020-03-11 12:45:48 -0500

    Added insert for all existing json files into tables

--- json_to_mongodb.py
+++ json_to_mongodb.py
@@ -1,18 +1,18 @@
 import pymongo
 import json
 
-#Set up the MongoClient and the connection
-client = pymongo.MongoClient("mongodb://localhost:27017/") #Connection will be established to the default host (localhost) and port (27017)
+#Set up the MongoClient and the connection to the default host (localhost) and port (27017)
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 #Create database
 db = client['yelp_database'] 
 
-#Create collection
+#Create collection checkins------------------------------------------------
 checkins = db['checkin'] 
 
 #Read from json file into a list
 checkin_list =[]
-for line in open('yelp_academic_dataset_checkin.json', 'r'):
+for line in open('Data\yelp_academic_dataset_checkin.json', encoding="utf8"):
     checkin_list.append(json.loads(line))
 
 #Delete all rows in collection
@@ -20,15 +20,74 @@ checkins.delete_many({})
 #Insert list into collection
 checkins.insert_many(checkin_list)
 
-#Query the collection
-cursor = checkins.find({})
+print("\nInserting checkins completed!")
+# #Query the collection
+# cursor = checkins.find({})
 
-#Print first five rows to check if inserted
-i=1
-for document in cursor:
-    if(i<5):
-        print(document)
-        i=i+1
-        
+# #Print first five rows to check if inserted
+# i=1
+# for document in cursor:
+#     if(i<5):
+#         print(document)
+#         i=i+1
+
+#Create collection businesses--------------------------------------------------
+businesses = db['business'] 
+
+#Read from json file into a list
+business_list =[]
+for line in open('Data\yelp_academic_dataset_business.json', encoding="utf8"):
+    business_list.append(json.loads(line))
+
+#Delete all rows in collection
+businesses.delete_many({})
+#Insert list into collection
+businesses.insert_many(business_list)
+print("\nInserting businesses completed!")
+
+#Create collection reviews------------------------------------------------------
+reviews = db['review'] 
+
+#Read from json file into a list
+review_list =[]
+for line in open('Data\yelp_academic_dataset_review.json', encoding="utf8"):
+    review_list.append(json.loads(line))
+
+#Delete all rows in collection
+reviews.delete_many({})
+#Insert list into collection
+reviews.insert_many(review_list)
+print("\nInserting reviews completed!")
+
+#Create collection tips---------------------------------------------------------
+tips = db['tip'] 
+
+#Read from json file into a list
+tip_list =[]
+for line in open('Data\yelp_academic_dataset_tip.json', encoding="utf8"):
+    tip_list.append(json.loads(line))
+
+#Delete all rows in collection
+tips.delete_many({})
+#Insert list into collection
+tips.insert_many(tip_list)
+print("\nInserting tips completed!")
+
+#Create collection users--------------------------------------------------------
+users = db['user'] 
+
+#Read from json file into a list
+user_list =[]
+for line in open('Data\yelp_academic_dataset_user.json', encoding="utf8"):
+    user_list.append(json.loads(line))
+
+#Delete all rows in collection
+users.delete_many({})
+#Insert list into collection
+users.insert_many(user_list)
+print("\nInserting users completed!")
+
 #Close connection
 client.close()
+
+
