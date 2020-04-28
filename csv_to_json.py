a5433d0cb4a643a04cd99ac1a5b9d3e243355281
rojinadeuja <rojinadeuja33g@gmail.com>
2020-04-27 19:12:39 -0500

    Added header and comments

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,7 +1,16 @@
+##################################################
+## {A script that converts csv files to json files and saves it on disk}
+##################################################
+## Author: {Rojina Deuja}
+## Last Updated: {04-27-2020}
+##################################################
+
+#Import necessary modules
 import json
 import pymongo
 import pandas as pd
 myclient = pymongo.MongoClient()
-
-df = pd.read_csv('Data\yelp_academic_dataset_review_10000.csv',encoding = 'utf8')   # loading csv file
-df.to_json('Data\yelp_academic_dataset_review_10000.json')                               # saving to json file
+#Read csv file into pandas dataframe
+df = pd.read_csv('csv_filename',encoding = 'utf8')
+#Covert to json and save to disk
+df.to_json('json_filename')                              
