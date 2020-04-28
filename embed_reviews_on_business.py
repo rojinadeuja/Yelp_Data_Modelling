c85ad2a7aa7ea42aad69c7cb31800af4b8ee4283
rojinadeuja <rojinadeuja33g@gmail.com>
2020-04-27 19:27:24 -0500

    Added header and comments

--- embed_reviews_on_business.py
+++ embed_reviews_on_business.py
@@ -1,29 +1,37 @@
+##################################################
+## {A script that embeds the review collection into business collection MongoDB}
+##################################################
+## Author: {Rojina Deuja}
+## Last Updated: {04-27-2020}
+##################################################
+
+# Import necessary modules
 import pymongo
 import json
 
-#Set up the MongoClient and the connection to the default host (localhost) and port (27017)
+# Set up the MongoClient and the connection to the default host (localhost) and port (27017)
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-#Create database
+# Open database and collections
 db = client['yelp_database'] 
 reviews = db['review']
 businesses = db['business']
 
-#Find documents from the collection
+# Find documents from the review collection
 myquery = {}
-mydoc = reviews.find(myquery)
+mydoc = reviews.find(myquery) # db.collection.find({})
 
-result = list(mydoc) #db.collection.find()
+result = list(mydoc) 
 cnt=0
 for x in result:
     print("Rows updated:", cnt)
-    business_id = x['business_id'] #x is a dict
+    business_id = x['business_id'] # x is a dict
     myquery = { "business_id": business_id}
     newvalues = {"$push": {"reviews" : x}}
     businesses.update(myquery, newvalues)
     cnt = cnt+1
 
-#Close connectioncls
+# Close connection
 client.close()
 
 
