fd8f6d02510be0d1f20acf6c2915f6e45f71c33b
rojinadeuja <rojinadeuja33g@gmail.com>
2020-04-28 21:01:01 -0500

    Added mongoimport for each collection

--- import_script.ps1
+++ import_script.ps1
@@ -11,7 +11,16 @@
 # Run the script: ./import_script.ps1
 
 # Import data into collection business
-mongoimport --db=yelp_database_test --collection=business --file=yelp_academic_dataset_business.json
+mongoimport --db=yelp_database --collection=business --file=Data/yelp_academic_dataset_business.json
 
 # Import data into collection review
-mongoimport --db=yelp_database_test --collection=review --file=yelp_academic_dataset_review.json
+mongoimport --db=yelp_database --collection=review --file=Data/yelp_academic_dataset_review.json
+
+# Import data into collection user
+mongoimport --db=yelp_database --collection=user --file=Data/yelp_academic_dataset_user.json
+
+# Import data into collection checkin
+mongoimport --db=yelp_database --collection=checkin --file=Data/yelp_academic_dataset_checkin.json
+
+# Import data into collection tip
+mongoimport --db=yelp_database --collection=tip --file=Data/yelp_academic_dataset_tip.json
